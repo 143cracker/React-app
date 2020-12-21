f49e9ae4337052d0b9c6ffeca819bb7560bc06e3
bansh <42534818+143cracker@users.noreply.github.com>
2020-12-22 02:26:59 +0530

    Update image.js

--- src/upload/image.js
+++ src/upload/image.js
@@ -19,7 +19,7 @@ fileUploadHandler=()=>
   this.setState({loading:true})
   data.append('image',this.state.selectedFile,this.state.selectedFile.name)
   const instance=axios.create({headers:{'Content-Type': 'application/json','x-access-token':'6961d7333843f1a7c280ffda57c5f3'}});
-  instance.get('http://uploadproject-env.eba-pezpkcpz.us-east-2.elasticbeanstalk.com/api/upload',data).then((resp)=>{
+  instance.get('api/upload',data).then((resp)=>{
   console.log(resp);
   this.setState({loading:false,
     message:resp.data.message})
